64a045fbee01d5b065a79c6ceef02d4cd3247526
Achmed Waly <hva@awaly.nl>
2020-09-28 01:17:33 +0200

    Added main layout for game fragment

--- app/src/main/java/nl/hva/fdmci/mad/level4_task2/GameFragment.kt
+++ app/src/main/java/nl/hva/fdmci/mad/level4_task2/GameFragment.kt
@@ -1,15 +1,13 @@
 package nl.hva.fdmci.mad.level4_task2
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.view.menu.ActionMenuItem
 import androidx.appcompat.widget.Toolbar
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -23,14 +21,21 @@ class GameFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_game, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_GameFragment_to_HistoryFragment)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_history -> {
+                findNavController().navigate(R.id.action_GameFragment_to_HistoryFragment)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
--- app/src/main/java/nl/hva/fdmci/mad/level4_task2/MainActivity.kt
+++ app/src/main/java/nl/hva/fdmci/mad/level4_task2/MainActivity.kt
@@ -6,7 +6,10 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.Toast
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -24,16 +27,4 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_history -> {
-                // TODO: Add navigation
-                return true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
